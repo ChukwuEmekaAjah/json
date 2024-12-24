880463c8025d5946bd352f8ba767bc8405824621
ChukwuEmekaAjah <talk2ajah@gmail.com>
2024-12-24 19:56:34 +0000

    Refactor code to use C++ namespace instead of class objects

--- json.cpp
+++ json.cpp
@@ -101,70 +101,9 @@ class JSONValue {
     }
 };
 
-class JSON {
+namespace JSON {
 
-  public:
-    string stringify(JSONValue value) {
-      if(value.getType() == "STRING"){
-        return "\"" + value.getString() + "\"";
-      } else if(value.getType() == "NUMBER"){
-        if((value.getNumber() - int(value.getNumber())) == 0){
-          return to_string(int(value.getNumber()));
-        }
-        return to_string(round(value.getNumber() * 10000.0) / 10000.0);
-      } else if(value.getType() == "BOOLEAN"){
-        return value.getBoolean() == true ? "true" : "false";
-      } else if(value.getType() == "NULL"){
-        return "null";
-      } else if(value.getType() == "ARRAY"){
-        string result = "[";
-        
-        for(int i = 0; i < value.arr.size(); i++){
-          if(i != 0){
-            result += ",";
-          }
-          result += stringify(value[i]);
-        }
-        result += "]";
-        return result;
-      } else {
-        string result = "{";
-        int counter = 0;
-        for(auto const& [key,val] : value.dict){
-          if(counter != 0){
-            result += ",";
-          }
-          result += "\"" + key + "\"";
-          result += " : ";
-          result += stringify(val);
-          counter += 1;
-        }
-        result += "}";
-        return result;
-      }
-    };
-
-    JSONValue parse(string value){
-      if(value.length() == 0){
-        throw runtime_error("Invalid JSON string provided.");
-      }
-      auto tokens = scanTokens(value);
-
-      // for(int i = 0; i < tokens.size(); i++){
-      //   std::cout<< "Token type is: " << tokens[i].tokenType << " and value is " << tokens[i].tokenValue << std::endl;
-      // }
-      // JSONValue t = parseTokens(tokens, 0);
-      // std::cout << "Number type is " << t.getNumber() <<  " is it " << endl;
-      int start = 0;
-      auto result = parseTokens(tokens, start);
-
-      if(start < tokens.size() - 1){
-        throw runtime_error("Invalid item " +  tokens[start].tokenValue + " in json string ");
-      }
-      return result;
-    };
-
-  private:
+  namespace{
     bool isNumeric(char c){
       return c >= '0' && c <= '9';
     }
@@ -237,15 +176,7 @@ class JSON {
 
     vector<JSONToken> scanTokens(string text){
       vector<JSONToken> tokens {};
-      /*
-        types:
-        number = 13.32
-        null = null
-        boolean = true | false
-        string = "dajfaj"
-        array = ["ajahd"]
-        object = {"ajah": "cute", "age": 23}
-      */
+
       for(int i = 0; i < text.length(); ){
         switch (text[i]){
         case '{': // opening brace for dicts
@@ -308,7 +239,7 @@ class JSON {
             auto response = consumeIdentifier(i, text);
             i = get<int>(response);
             string value = get<string>(response);
-            tokens.push_back(JSONToken( value == "null" ? "NULL" : "BOOLEAN", value));
+            tokens.push_back(JSONToken( value == "null" ? "NULL" : "BOOLEAN", value == "null" ? "\0" : value));
           } 
           break;
         }
@@ -318,7 +249,7 @@ class JSON {
     };
 
     JSONValue parseTokens(vector<JSONToken> tokens, int& i){
-      
+
       JSONToken token = tokens[i];
 
       if(token.tokenType == "STRING"){
@@ -403,39 +334,60 @@ class JSON {
       }
     }
 
+  }
+
+  string stringify(JSONValue value) {
+    if(value.getType() == "STRING"){
+      return "\"" + value.getString() + "\"";
+    } else if(value.getType() == "NUMBER"){
+      if((value.getNumber() - int(value.getNumber())) == 0){
+        return to_string(int(value.getNumber()));
+      }
+      return to_string(round(value.getNumber() * 10000.0) / 10000.0);
+    } else if(value.getType() == "BOOLEAN"){
+      return value.getBoolean() == true ? "true" : "false";
+    } else if(value.getType() == "NULL"){
+      return "null";
+    } else if(value.getType() == "ARRAY"){
+      string result = "[";
+      
+      for(int i = 0; i < value.arr.size(); i++){
+        if(i != 0){
+          result += ",";
+        }
+        result += stringify(value[i]);
+      }
+      result += "]";
+      return result;
+    } else {
+      string result = "{";
+      int counter = 0;
+      for(auto const& [key,val] : value.dict){
+        if(counter != 0){
+          result += ",";
+        }
+        result += "\"" + key + "\"";
+        result += " : ";
+        result += stringify(val);
+        counter += 1;
+      }
+      result += "}";
+      return result;
+    }
+  };
+
+  JSONValue parse(string value){
+    if(value.length() == 0){
+      throw runtime_error("Invalid JSON string provided.");
+    }
+    auto tokens = scanTokens(value);
+
+    int start = 0;
+    auto result = parseTokens(tokens, start);
+
+    if(start < tokens.size() - 1){
+      throw runtime_error("Invalid item " +  tokens[start].tokenValue + " in json string ");
+    }
+    return result;
+  };
 };
-
-// int main(int argc, char* argv[]){
-//   JSON parser {};
-//   //"{\"age\":-23.32, \"is_man\": true, \"who\": null, \"name\": {\"last\": \"Ajah\", \"first\": [\"Chuks\"]}}"
-//   auto t = parser.parse(argv[1]);
-//   // cout << t["age"].getNumber() << " that is the age" << endl;
-//   // cout << t["name"].getString() << " that is the name" << endl;
-//   // cout << "first item is " << t[0].getNumber() << endl;
-//   // cout << "3rd item is " << t[2].getString() << endl;
-//   // cout << "the key to my heart is " << t[3][2]["key"]["key"].getString() << endl;
-//   // vector<JSONValue> arr {};
-//   // arr.push_back(JSONValue("STRING", "Chuks"));
-//   // arr.push_back(JSONValue("STRING", "Mercy"));
-//   // arr.push_back(JSONValue("STRING", "Grace"));
-//   // map<string, JSONValue> family{};
-//   // family["children"] = JSONValue("ARRAY", arr);
-//   // family["father"] = JSONValue("STRING", "Patrick");
-//   // family["mother"] = JSONValue("STRING", "Priscillia");
-//   // family["is_wealthy"] = JSONValue("BOOLEAN", "true");
-//   // JSONValue j = JSONValue("OBJECT", family);
-//   // JSONValue s = JSONValue("STRING", "Chuks");
-//   // cout << "First child is " << j["children"][0].getString() << endl;
-
-//   // cout << "First child from array is " << arr[0].data_type << endl;
-
-//   // cout << "First child from STRING is " << j.getType() << endl;
-//   cout << "output of stringify is" << parser.stringify(t) << endl;
-//   // JSONValue s = JSONValue("STRING", string("CHUKS"));
-//   //   JSONValue n = JSONValue("NUMBER", 23.322);
-//   //   JSONValue b = JSONValue("BOOLEAN", true);
-//   //   cout << " Value is " << s.str_value<< endl;
-//   //   cout << " Value for num is " << n.d_value << endl;
-//   //   cout << " Value for bool is " << b.b_value << endl;
-//   return 0;
-// }
--- json_test.cpp
+++ json_test.cpp
@@ -3,27 +3,26 @@
 #include "json.cpp"
 
 int main(){
-  JSON parser {};
   
-  auto t = parser.parse("\"name\"");
+  auto t = JSON::parse("\"name\"");
   assert(t.getString() == "name");
   std::cout << "Test that a simple string is a valid json was successful" << std::endl;
   
-  t = parser.parse("23");
+  t = JSON::parse("23");
   assert(t.getNumber() == 23);
   std::cout << "Test that a simple integer number is a valid json was successful" << std::endl;
   
-  t = parser.parse("23.32");
+  t = JSON::parse("23.32");
   assert(t.getNumber() == 23.32);
   std::cout << "Test that a simple real number is a valid json was successful" << std::endl;
   
 
-  t = parser.parse("23.32E2");
+  t = JSON::parse("23.32E2");
   assert(t.getNumber() == 2332);
   std::cout << "Test that a simple exponential number is a valid json was successful" << std::endl;
 
 
-  t = parser.parse("[78,\"json\"]");
+  t = JSON::parse("[78,\"json\"]");
   assert(t[0].getNumber() == 78);
   assert(t[1].getString() == "json");
   std::cout << "Test that an array containing multiple types is a valid json was successful" << std::endl;
